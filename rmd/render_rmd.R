bf097e099627d2b8b5a5534104c096089f7dcba0
Micaela Chan <4413270+mychan24@users.noreply.github.com>
2020-07-02 16:05:14 -0500

    update make file

--- rmd/render_rmd.R
+++ rmd/render_rmd.R
@@ -1,2 +1,2 @@
-rmarkdown::render(input = "./covid_dallas_trend.Rmd",
-                  output_file = "./docs/index.html")
+rmarkdown::render(input = "rmd/covid_dallas_trend.Rmd",
+                  output_file = "docs/index.html")
