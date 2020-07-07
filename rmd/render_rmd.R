a9c9bf24b9ed4c8b3c7d06123a5c784c2d3cad78
Micaela Chan <4413270+mychan24@users.noreply.github.com>
2020-07-07 16:01:37 -0500

    Make dash-board version of report using flex_dashboard, spell check

--- rmd/render_rmd.R
+++ rmd/render_rmd.R
@@ -1,1 +1,1 @@
-rmarkdown::render(input = "rmd/covid_dallas_trend.Rmd", output_file = "../docs/index.html")
+rmarkdown::render(input = "rmd/covid_dallas_trend_dashboard.Rmd", output_file = "../docs/index.html")
